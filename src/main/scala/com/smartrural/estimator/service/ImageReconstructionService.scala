110b4b07fee83a7b3bb43b64e63d38b080f5464d
MoyanoJ <jorge.moyano@vodafone.com>
2018-01-27 10:53:59 +0000

    Adding tests for the image reconstruction service

--- src/main/scala/com/smartrural/estimator/service/ImageReconstructionService.scala
+++ src/main/scala/com/smartrural/estimator/service/ImageReconstructionService.scala
@@ -4,16 +4,17 @@ import java.awt.image.BufferedImage
 import java.io.{File, FileInputStream, FilenameFilter}
 import javax.imageio.ImageIO
 
+import com.smartrural.estimator.AppConstants
 import com.smartrural.estimator.file.BoundingBoxTextReader
-import com.smartrural.estimator.model.InferenceInfo
+import com.smartrural.estimator.model.{InferenceInfo, RGBPixel}
 
 object ImageReconstructionService {
 
-  def reconstructImage(originalImageFile:File, bboxesInfoFile:File, patchesPath:String, destinationPath:String):Boolean = {
+  def reconstructImage(originalImageFile:File, bboxesInfoFile:File, patchesPath:File, destinationPath:String):Boolean = {
     val partitionFolder = originalImageFile.getParentFile.getName
     val originalImage = ImageIO.read(new FileInputStream(originalImageFile))
     val patchesInfoList = BoundingBoxTextReader.getFilteredInferenceInfo(bboxesInfoFile, originalImageFile.getName)
-    val patchesImages = retrievePatchesForImage(patchesPath, originalImageFile.getName)
+    val patchesImages = retrievePatchesForImage(patchesPath.getPath, originalImageFile.getName)
 
     createCompleteBinaryImage(
       patchesImages.zip(patchesInfoList.toList),
@@ -26,7 +27,7 @@ object ImageReconstructionService {
     new File(patchesPath)
       .listFiles(new FilenameFilter {
         override def accept(dir: File, name: String): Boolean =
-          name.startsWith(imageName.substring(0, imageName.lastIndexOf(".")))
+          name.startsWith(imageName.substring(0, imageName.lastIndexOf(".png")))
       }).map(patchFile => ImageIO.read(new FileInputStream(patchFile))).toList
 
   def createCompleteBinaryImage(patchImageInfo: List[(BufferedImage, InferenceInfo)],
@@ -35,8 +36,13 @@ object ImageReconstructionService {
     patchImageInfo.foreach { case (image, info) =>
         for (y <- info.yMin to info.yMax;
               x <- info.xMin to info.xMax)
-        destinationImage.setRGB(x, y, image.getRGB(x, y))
+          writePixel(x, y, image.getRGB(x,y), destinationImage§)
     }
     if(!patchImageInfo.isEmpty) ImageIO.write(destinationImage, "png", destinationFile) else false
+  }
+
+  def writePixel(xCoordinate:Int, yCoordinate:Int, rgb:Int, destination: BufferedImage):Unit = {
+    if (new RGBPixel(rgb) != AppConstants.VoidRGB)
+      destination.setRGB(xCoordinate, yCoordinate, rgb)
   }
 }
--- src/test/scala/com/smartrural/estimator/service/ImageReconstructionServiceTest.scala
+++ src/test/scala/com/smartrural/estimator/service/ImageReconstructionServiceTest.scala
@@ -3,6 +3,7 @@ package com.smartrural.estimator.service
 import java.io.File
 import javax.imageio.ImageIO
 
+import com.smartrural.estimator.AppConstants
 import org.junit.runner.RunWith
 import org.scalatest.FlatSpec
 import org.scalatest.junit.JUnitRunner
@@ -12,14 +13,17 @@ import org.scalatest.junit.JUnitRunner
 class ImageReconstructionServiceTest extends FlatSpec{
 
   val rootPath = getClass.getClassLoader.getResource(".").getPath
-  val patchesFolder = new File(new File(rootPath), "ingerences/valdemonjas-2017-09-13_01")
-  val image = ImageIO.read(getClass.getClassLoader.getResourceAsStream("./image.png"))
+  val patchesFolder = new File(new File(rootPath), "inferences/valdemonjas-2017-09-13_01")
+  val originalImagesFolder =  new File(rootPath.concat("original_images/valdemonjas-2017-09-13_01"))
+  val bboxesFolder =  new File(rootPath.concat("inferences_info/valdemonjas-2017-09-13_01"))
+  val imageName = "z-img-000-000004.png"
+  val image = ImageIO.read(getClass.getClassLoader.getResourceAsStream("./"))
 
   behavior of "ImageReconstructionService"
 
   it should "find the related images within the inferences folder" in {
     val bufferedImageArray =
-      ImageReconstructionService.retrievePatchesForImage(patchesFolder.getPath, "z-img-000-000004.png")
+      ImageReconstructionService.retrievePatchesForImage(patchesFolder.getPath, imageName)
     assert(bufferedImageArray.size == 7)
     assert(bufferedImageArray(0).getWidth == 91)
     assert(bufferedImageArray(0).getHeight == 145)
@@ -37,13 +41,26 @@ class ImageReconstructionServiceTest extends FlatSpec{
     assert(bufferedImageArray(6).getHeight == 52)
   }
 
-  it should "reconstruct the image from the available patches" in {
+  it should "reconstruct the final image from the available patches" in {
+    ImageReconstructionService.reconstructImage(
+      new File(originalImagesFolder, imageName),
+      new File(bboxesFolder, AppConstants.BbBoxesFileName),
+      patchesFolder,
+      rootPath
+    )
 
+    assert(new File(rootPath, imageName).exists())
   }
 
   it should "recreate the binary image from the patches" in {
 
   }
 
-  it should "fail if the "
+  it should "write the pixel with the specified value" in {
+
+  }
+
+  it should "not write the pixel if it is void" in {
+
+  }
 }
